c136b3f6b25d5b784142e0fed2c01f73507a4e17
Jaakko Heusala <jhh@heusalagroup.fi>
2022-08-10 01:25:43 +0300

    Added test

--- utils/WorkspaceUtils.test.ts
+++ utils/WorkspaceUtils.test.ts
@@ -1,7 +1,12 @@
+// Copyright (c) 2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 // Copyright (c) 2021. Sendanor <info@sendanor.fi>. All rights reserved.
 
 import { WorkspaceUtils } from "./WorkspaceUtils";
 
-describe('WorkspaceUtils', () => {
+describe("WorkspaceUtils", () => {
+
+    it("is class", () => {
+        expect(WorkspaceUtils).toBeClass();
+    })
 
 });
